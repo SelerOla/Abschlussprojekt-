68490d71b9529ac0d160e9e6f0c9428a014374b5
Tankis-55 <129220867+Tankis-55@users.noreply.github.com>
2024-11-19 16:51:36 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ app.config['UPLOAD_FOLDER'] = 'static'
 def home():
     return '<h1>Welcome to the COVID Dashboard</h1> <img src="/static/covid_dashboard.png" alt="COVID Dashboard">'
 
-@app.route('/static/<filename>')
-def send_image(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+@app.route('/static/<covid_dashboard>')
+def send_image(covid_dashboard):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], covid_dashboard)
 
 if __name__ == '__main__':
     app.run(debug=True)
